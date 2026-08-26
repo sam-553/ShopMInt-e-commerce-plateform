15bacd3e14e662522dab804df8e6c39c2afdb678
sampandit553 <sameertiwari737697@gmail.com>
2026-08-26 13:26:53 +0530

    fix api

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 5000;
 
 
 const allowedOrigins = [
-  "https://shopmint-client.vercel.app",
+  "https://shopmint-frontend.vercel.app/",
   "http://localhost:5173",
   "http://localhost:3000",
 ];
